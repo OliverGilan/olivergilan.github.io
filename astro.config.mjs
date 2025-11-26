b6ecb5b4498f88a33a381b0c4bb37089bf844c0f
Oliver Gilan <github@olivergilan.com>
2025-11-25 21:59:32 -0500

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://olivergilan.com", // Replace with your actual site URL
-  integrations: [mdx()],
+  site: "https://olivergilan.com",
+  integrations: [mdx(), tailwind()],
 });
