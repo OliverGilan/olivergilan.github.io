78fce4cd9eef12d25edb05e63d243a856dfb72ad
Oliver Gilan <github@olivergilan.com>
2025-11-25 19:59:22 -0500

    astro

---
+++ src/content.config.ts
@@ -0,0 +1,9 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from "astro/loaders";
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/writings" }),
+});
+
+export const collections = { blog };
