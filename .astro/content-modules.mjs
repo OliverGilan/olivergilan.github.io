f4a3159a261aadc121e7af4603672d7afdf736e4
Oliver Gilan <github@olivergilan.com>
2025-12-27 16:39:13 -0800

    migrate all post files

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,13 @@
 
 export default new Map([
 ["src/posts/003-prioritizing-efforts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F003-prioritizing-efforts.mdx&astroContentModuleFlag=true")],
-["src/posts/018-what-the-nfl-taught-me-about-business.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F018-what-the-nfl-taught-me-about-business.mdx&astroContentModuleFlag=true")],
 ["src/posts/009-software-crisis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F009-software-crisis.mdx&astroContentModuleFlag=true")],
-["src/posts/023-cloud-hasnt-been-won.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F023-cloud-hasnt-been-won.mdx&astroContentModuleFlag=true")],
 ["src/posts/001-how-to-create-a-blog-in-hugo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F001-how-to-create-a-blog-in-hugo.mdx&astroContentModuleFlag=true")],
-["src/posts/002-work-life-balance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F002-work-life-balance.mdx&astroContentModuleFlag=true")],
-["src/posts/004-internal-knowledge-systems.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F004-internal-knowledge-systems.mdx&astroContentModuleFlag=true")]]);
+["src/posts/014-living-with-buggy-hardware.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F014-living-with-buggy-hardware.mdx&astroContentModuleFlag=true")],
+["src/posts/018-what-the-nfl-taught-me-about-business.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F018-what-the-nfl-taught-me-about-business.mdx&astroContentModuleFlag=true")],
+["src/posts/019-goals-for-2024.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F019-goals-for-2024.mdx&astroContentModuleFlag=true")],
+["src/posts/020-kidney-crisis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F020-kidney-crisis.mdx&astroContentModuleFlag=true")],
+["src/posts/021-full-gluten-tolerance.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F021-full-gluten-tolerance.mdx&astroContentModuleFlag=true")],
+["src/posts/023-cloud-hasnt-been-won.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F023-cloud-hasnt-been-won.mdx&astroContentModuleFlag=true")],
+["src/posts/025-resetting-the-frame.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fposts%2F025-resetting-the-frame.mdx&astroContentModuleFlag=true")]]);
 		
