27e0b66832b7038e700774c98068a7df831f7493
Oliver Gilan <github@olivergilan.com>
2025-12-27 21:38:42 -0800

    address issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://olivergilan.com",
+  output: "static",
+  trailingSlash: "never",
+  build: {
+    format: "file",
+  },
   integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
